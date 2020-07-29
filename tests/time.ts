2be726c0aa0c8829e5abbd21375e3353e2a4c363
remorses <beats.by.morse@gmail.com>
2020-07-29 14:46:04 +0200

    tests/time: comments

--- tests/time.ts
+++ tests/time.ts
@@ -3,9 +3,9 @@ import { formatDuration, Beats, Fps } from '../src/time'
 
 describe('formatDuration', () => {
     it('', () => {
-        assert.equal(formatDuration(0.03336666666666667), `1001/30000s`)
+        assert.equal(formatDuration(0.03336666666666667), `1001/30000s`) // from the apple docs
         assert.equal(formatDuration(1), `1s`)
-        assert.equal(formatDuration(Beats(128, 128)), `7680000/128000s`)
-        assert.equal(formatDuration(Fps(25, 25)), '25000/25000s')
+        assert.equal(formatDuration(Beats(128, 128)), `7680000/128000s`) // 60 seconds
+        assert.equal(formatDuration(Fps(25, 25)), '25000/25000s') // 1 second
     })
 })
