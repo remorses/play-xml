840188d1fb5d12d354d6b34529ab7d5a69cf5e8c
remorses <beats.by.morse@gmail.com>
2020-07-30 15:56:39 +0200

    projects/ProjectFromClips: nn

--- src/projects/ProjectFromClips.tsx
+++ src/projects/ProjectFromClips.tsx
@@ -2,7 +2,12 @@ import { JsxElement, JSXXML, render } from 'jsx-xml'
 import { Asset, Clip, Project } from '../components'
 import { TimeObject } from '../time'
 import { uniqBy } from 'lodash'
-import { downloadFile, getVideoFormat, getVideoInfo, downloadM3u8 } from '../support'
+import {
+    downloadFile,
+    getVideoFormat,
+    getVideoInfo,
+    downloadM3u8,
+} from '../support'
 
 export const ProjectFromVideos = ({
     videos,
@@ -21,10 +26,10 @@ export const ProjectFromVideos = ({
     )
 }
 
-export const projectFromUrls = async ({ urls, ...rest }) => {
+export const projectFromUrls = async ({ urls, directory = '', ...rest }) => {
     let videos = []
     for (let url of urls) {
-        const { filePath, unlink } = await downloadM3u8({ url,  })
+        const { filePath, unlink } = await downloadM3u8({ url, directory })
         console.log('finish download of ' + filePath)
         const info = await getVideoInfo(filePath)
         // console.log(info)
